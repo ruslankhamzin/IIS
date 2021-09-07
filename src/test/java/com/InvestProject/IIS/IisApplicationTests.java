8e98fc3fa26724f761158403cb78651c468ccab3
ruslik111 <hamzin362@gmail.com>
2021-09-07 22:23:03 +0300

    Initial commit

---
+++ src/test/java/com/InvestProject/IIS/IisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InvestProject.IIS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
